6d7ef94f71e0e2ad507b2911af286234cdf0e3e8
Sagaris88 <auto_enthusiast@hotmail.com>
2024-05-12 22:29:56 -0400

    #50 Beoing

---
+++ public/js/changeImage.js
@@ -0,0 +1,230 @@
+
+/* Change image */
+
+/* 737 Original */
+
+function changeImage737100() {
+    if (document.getElementById("737original").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20737-100.png"){
+        document.getElementById("737original").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20737-100.png";
+    }
+}
+
+function changeImage737200() {
+    if (document.getElementById("737original").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20737-200.png"){
+        document.getElementById("737original").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20737-200.png";
+    }
+}
+
+/* 737 Classic */
+
+function changeImage737300() {
+    if (document.getElementById("737classic").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20737-300.png"){
+        document.getElementById("737classic").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20737-300.png";
+    }
+}
+
+function changeImage737500() {
+    if (document.getElementById("737classic").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20737-500.png"){
+        document.getElementById("737classic").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20737-500.png";
+    }
+}
+
+/* 737 NG */
+
+function changeImage737600() {
+    if (document.getElementById("737ng").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20737-600.png"){
+        document.getElementById("737ng").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20737-600.png";
+    }
+}
+
+function changeImage737800() {
+    if (document.getElementById("737ng").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20737-800.png"){
+        document.getElementById("737ng").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20737-800.png";
+    }
+}
+
+function changeImage737900() {
+    if (document.getElementById("737ng").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20737-900.png"){
+        document.getElementById("737ng").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20737-900.png";
+    }
+}
+
+/* 737 MAX */
+
+function changeImage7378() {
+    if (document.getElementById("737max").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20737%20MAX%208.png"){
+        document.getElementById("737max").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20737%20MAX%208.png";
+    }
+}
+
+function changeImage7379() {
+    if (document.getElementById("737max").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20737%20MAX%209.png"){
+        document.getElementById("737max").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20737%20MAX%209.png";
+    }
+}
+
+/* 747 Original */
+
+function changeImage747100() {
+    if (document.getElementById("747original").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20747-100.png"){
+        document.getElementById("747original").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20747-100.png";
+    }
+}
+
+function changeImage747200() {
+    if (document.getElementById("747original").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20747-200.png"){
+        document.getElementById("747original").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20747-200.png";
+    }
+}
+
+function changeImage747SP() {
+    if (document.getElementById("747original").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20747%20SP.png"){
+        document.getElementById("747original").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20747%20SP.png";
+    }
+}
+
+/* 747 Original */
+
+function changeImage747300() {
+    if (document.getElementById("747new").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20747-300.png"){
+        document.getElementById("747new").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20747-300.png";
+    }
+}
+
+function changeImage747400() {
+    if (document.getElementById("747new").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20747-400.png"){
+        document.getElementById("747new").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20747-400.png";
+    }
+}
+
+function changeImage7478i() {
+    if (document.getElementById("747new").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20747-8.png"){
+        document.getElementById("747new").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20747-8.png";
+    }
+}
+
+/* 757 */
+
+function changeImage757200() {
+    if (document.getElementById("757").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20757-200.png"){
+        document.getElementById("757").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20757-200.png";
+    }
+}
+
+function changeImage757300() {
+    if (document.getElementById("757").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20757-300.png"){
+        document.getElementById("757").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20757-300.png";
+    }
+}
+
+/* 767 */
+
+function changeImage767200() {
+    if (document.getElementById("767").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20767-200.png"){
+        document.getElementById("767").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20767-200.png";
+    }
+}
+
+function changeImage767300() {
+    if (document.getElementById("767").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20767-300.png"){
+        document.getElementById("767").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20767-300.png";
+    }
+}
+
+function changeImage767400() {
+    if (document.getElementById("767").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20767-400.png"){
+        document.getElementById("767").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20767-400.png";
+    }
+}
+
+/* 777 */
+
+function changeImage777200() {
+    if (document.getElementById("777").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20777-200.png"){
+        document.getElementById("777").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20777-200.png";
+    }
+}
+
+function changeImage777300() {
+    if (document.getElementById("777").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20777-300.png"){
+        document.getElementById("777").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20777-300.png";
+    }
+}
+
+/* 777 */
+
+function changeImage7778() {
+    if (document.getElementById("777max").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20777-8.png"){
+        document.getElementById("777max").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20777-8.png";
+    }
+}
+
+function changeImage7779() {
+    if (document.getElementById("777max").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20777-9.png"){
+        document.getElementById("777max").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20777-9.png";
+    }
+}
+
+/* 787 */
+
+function changeImage7878() {
+    if (document.getElementById("787").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20787-8.png"){
+        document.getElementById("787").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20787-8.png";
+    }
+}
+
+function changeImage7879() {
+    if (document.getElementById("787").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20787-9.png"){
+        document.getElementById("787").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20787-9.png";
+    }
+}
+
+function changeImage78710() {
+    if (document.getElementById("787").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20787-10.png"){
+        document.getElementById("787").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20787-10.png";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function changeImage787() {
+    if (document.getElementById("787-9").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20787-9.png"){
+        document.getElementById("787-9").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20787-9.png";
+    }
+}
+
+function changeImage777() {
+    if (document.getElementById("787-9").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20777-300.png"){
+        document.getElementById("787-9").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20777-300.png";
+    }
+}
+
+function changeImage767() {
+    if (document.getElementById("787-9").src !== "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20767-300.png"){
+        document.getElementById("787-9").src = "https://storage.googleapis.com/car-silhouette-1/Boeing/Boeing%20767-300.png";
+    }
+}
